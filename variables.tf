8b22d6a9e62d6ffd865f6b1874d8fc56f778673e
Sathish Shankar <168322001+Sathishshankar13@users.noreply.github.com>
2025-09-27 05:24:54 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,8 @@ variable "additional_policy_name" {
   default     = "loadbalancer-policy"
 }
 
+variable "existing_igw_id" {
+  description = "ID of existing internet gateway attached to the VPC"
+  type        = string
+  default     = "igw-072bc09258e3f7347"
+}
