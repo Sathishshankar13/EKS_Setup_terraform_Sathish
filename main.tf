8de8312e6f7091379a9b7afebc97a0dda3c63f4b
Sathish Shankar <168322001+Sathishshankar13@users.noreply.github.com>
2025-09-27 04:27:33 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,198 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Get the default VPC details
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default_vpc.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = [
+      "us-east-1a",
+      "us-east-1b",
+    ]
+  }
+}
+
+
+# Internet Gateway attached to default VPC
+resource "aws_internet_gateway" "default_igw" {
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  tags = {
+    Name = "default-vpc-igw"
+  }
+}
+
+# Route Table for public subnets
+resource "aws_route_table" "public_rt" {
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default_igw.id
+  }
+
+  tags = {
+    Name = "default-public-rt"
+  }
+}
+
+# Associate Route Table with public subnets
+resource "aws_route_table_association" "public_rta" {
+  count          = length(data.aws_subnets.public.ids)
+  subnet_id      = element(data.aws_subnets.public.ids, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group for EKS Cluster
+resource "aws_security_group" "cluster_sg" {
+  name   = "default-cluster-sg"
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "default-cluster-sg"
+  }
+}
+
+# Security Group for EKS Worker Nodes
+resource "aws_security_group" "node_sg" {
+  name   = "default-node-sg"
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "default-node-sg"
+  }
+}
+
+# IAM Role for EKS Cluster (You must define this)
+resource "aws_iam_role" "devopsshack_cluster_role" {
+  name = "devopsshack-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "eks.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.devopsshack_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# EKS Cluster resource with correct subnet and security group references
+resource "aws_eks_cluster" "devopsshack" {
+  name     = "devopsshack-cluster"
+  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = data.aws_subnets.public.ids
+    security_group_ids = [aws_security_group.cluster_sg.id]
+  }
+}
+
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name = aws_eks_cluster.devopsshack.name
+  addon_name   = "aws-ebs-csi-driver"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
+
+
+resource "aws_eks_node_group" "devopsshack" {
+  cluster_name    = aws_eks_cluster.devopsshack.name
+  node_group_name = "devopsshack-node-group"
+  node_role_arn   = aws_iam_role.devopsshack_worker_node_role.arn
+  subnet_ids      = data.aws_subnets.public.ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  instance_types = ["t2.medium"]
+
+  remote_access {
+    ec2_ssh_key               = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.node_sg.id]
+  }
+}
+
+# IAM Role for EKS Worker Nodes
+resource "aws_iam_role" "devopsshack_node_group_role" {
+  name = "devopsshack-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "devopsshack-node-group-role"
+  }
+}
+
+# Attach Required Policies to Node Group IAM Role
+
+resource "aws_iam_role_policy_attachment" "devopsshack_node_group_worker_policy" {
+  role       = aws_iam_role.devopsshack_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
+  role       = aws_iam_role.devopsshack_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
+  role       = aws_iam_role.devopsshack_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "devopsshack_node_group_ebs_policy" {
+  role       = aws_iam_role.devopsshack_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
